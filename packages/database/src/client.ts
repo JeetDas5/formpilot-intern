4a7c03c5a299b7461bd6832d5c2d84b2312d28b0
JeetDas5 <jeet15083011@gmail.com>
2025-04-24 12:34:24 +0530

    fixing prisma client 2

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,8 +1,9 @@
-import { PrismaClient } from "../generated/prisma";
+// lib/prisma.ts
+import { PrismaClient } from "../generated/prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient();
+  globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
